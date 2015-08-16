4f38bf7154d96dfb1434c220b448cce0dc8838da
Lei Lu <lulei.wm@gmail.com>
2015-08-16 15:55:20 -0700

    change user program to 'user_dumbfork' ... done

--- kernel/init.c
+++ kernel/init.c
@@ -59,10 +59,7 @@ i386_init(void)
 	// Starting non-boot CPUs
 	boot_aps();
 
-	//ENV_CREATE(user_dumbfork, ENV_TYPE_USER);
-	ENV_CREATE(user_hello, ENV_TYPE_USER);
-	ENV_CREATE(user_hello, ENV_TYPE_USER);
-	ENV_CREATE(user_hello, ENV_TYPE_USER);
+	ENV_CREATE(user_dumbfork, ENV_TYPE_USER);
 
 	// Schedule and run the first user environment
 	sched_yield();
